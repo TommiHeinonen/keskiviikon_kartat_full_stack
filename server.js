6e9c63a4c27320fd10902198ea73cc9c8d0e26bd
Your Name <you@example.com>
2020-09-30 19:30:29 +0300

    serveri lisätty

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
 console.log("Moikka moiii")
+
+const express = require('express')
+const app = express()
+//const port = 3000
+app.listen(3000, () => console.log ("kuuntelen"));
+app.use(express.static("public"));
+console.log("Serveri kaynissa!");
