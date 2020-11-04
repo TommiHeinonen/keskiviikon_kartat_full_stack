c6b1ab2501c548027f40bc2b559cdee075767aaa
Your Name <you@example.com>
2020-11-04 19:32:00 +0200

    postilaatikot valmiit

--- public/js/pankkikorttitiedot.js
+++ public/js/pankkikorttitiedot.js
@@ -4,6 +4,34 @@ var pin = document.getElementById("pin").value;
 
 console.log(nimi);
 console.log(pin);
+
+const data = {nimi, pin}
+
+
+
+ const options = {
+        method: "POST",
+        headers: {
+           "Content-Type":"application/json"
+
+        },
+        body: JSON.stringify(data)
+ };
+  fetch('/pankkitietojen_talletus', options).then(function(response) {
+    if(response.status == 200){
+      console.log("ok")
+    }
+  }, function(error){
+    console.log(error.message);
+  });
+
+
+
+
+
+
+
+
 }
 
 
--- server.js
+++ server.js
@@ -6,13 +6,22 @@ const app = express()
 app.listen(3000, () => console.log ("kuuntelen"));
 app.use(express.static("public"));
 console.log("Serveri kaynissaaaaa!");
+
+ app.use(express.json({limit: '1mb'}));
+
 const pankkikorttitiedot = [
    {
      "nimi": "Tomsku tomaatti",
-     "Kortti": "0142"
+     "pin": "0142"
    }
 ]
 
 app.get('/turvallinenpankintiedotpankille', function (req, res) {
   res.send(pankkikorttitiedot)
+})
+
+app.post('/pankkitietojen_talletus', function (req, res) {
+pankkikorttitiedot.push(req.body);
+ console.log(pankkikorttitiedot);
+ res.send(200);
 })
